41131af414eb6748465c29fe4416ba6c16241496
Fabio2222 <fabiomoreira73@outlook.com>
2022-02-23 16:01:26 -0300

    Adicionar arquivos de projeto.

---
+++ Tabuada_Nove/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Tabuada_Nove
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero, i;
+            int resultado;
+
+            
+            do
+            {
+                Console.WriteLine("Digite um numero de 1 a 9! ");
+                numero = int.Parse(Console.ReadLine());
+            } while (numero < 1 || numero > 9);        
+                                   
+                       
+                for (i = 0; i <= 10; i++)
+                {
+                    resultado = numero * i;
+                    Console.WriteLine("O resultado da multiplicacao por {0} e {1}", i, resultado);
+                }
+                       
+        }
+    }
+}
